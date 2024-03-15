9368a36dccc382a8b998a595b7baf1bd68ddb1b3
Jeff Davis <jeff.davis@gmail.com>
2024-03-15 11:09:12 -0700

    highlight marin in plot

--- county_map_ex.R
+++ county_map_ex.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(sf)
 library(geodata)
+library(osmdata)
+
 
 bbox <- getbb('Marin County, CA')
 
@@ -8,8 +10,8 @@ bbox <- getbb('Marin County, CA')
 x <- gadm("USA", level=2, path="tmp")
 
 ## crop to the marin bbox
-xsf <- st_crop(st_as_sf(x),xmin=bbox[1,1], ymin=bbox[2,1], xmax=bbox[1,2], ymax=bbox[2,2])
+xsf <- st_crop(st_as_sf(x),
+               xmin=bbox[1,1], ymin=bbox[2,1],
+               xmax=bbox[1,2], ymax=bbox[2,2])
 
 ggplot(xsf) + geom_sf() + geom_sf(data=xsf[xsf$NAME_2=="Marin",], fill="red")
-
-
